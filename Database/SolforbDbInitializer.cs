0837a17058d914746ac5a02055a669d5756bf356
Ahri-ninetailed <vadim.halyapin@yandex.ru>
2023-03-10 16:35:12 +0300

    Initialize db

---
+++ Database/SolforbDbInitializer.cs
@@ -0,0 +1,28 @@
+﻿using Database.Models;
+
+namespace Database
+{
+    public static class SolforbDbInitializer
+    {
+        /// <summary>
+        /// Инициализирует бд начальными значениями
+        /// </summary>
+        /// <param name="context"></param>
+        public static void Initialize(SolforbDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Providers.Any())
+                return;
+
+            var providers = new Provider[]
+            {
+                new Provider { Name = "RedTable" },
+                new Provider { Name = "BlackWater" },
+            };
+            foreach (var provider in providers)
+                context.Providers.Add(provider);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ SolforbTest/Extensions/MiddlewareExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Database;
+
+namespace SolforbTest.Extensions
+{
+    public static class MiddlewareExtensions
+    {
+        public static void CreateDbIfNotExists(this WebApplication app)
+        {
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<SolforbDbContext>();
+                    SolforbDbInitializer.Initialize(context);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Ошибка при создании БД:\n{ex.Message}");
+                }
+            }
+        }
+    }
+}
