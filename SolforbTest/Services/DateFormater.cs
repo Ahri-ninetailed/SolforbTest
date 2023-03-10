4e4fd9eaf955548e7fca4724331070920169762f
Ahri-ninetailed <vadim.halyapin@yandex.ru>
2023-03-10 19:25:56 +0300

    Extract date formatting functions

---
+++ SolforbTest/Services/DateFormater.cs
@@ -0,0 +1,26 @@
+﻿namespace SolforbTest.Services
+{
+    public static class DateFormater
+    {
+        public static string GetMonthAgoDateString()
+        {
+            DateTime monthAgoDate = DateTime.Now.AddMonths(-1);
+            string day = getCorrectDayOrMonth(monthAgoDate.Day);
+            string month = getCorrectDayOrMonth(monthAgoDate.Month);
+            string date = $"{monthAgoDate.Year}-{month}-{day}";
+            return date;
+        }
+        public static string GetTodayDateString()
+        {
+            DateTime todayDate = DateTime.Now;
+            string day = getCorrectDayOrMonth(todayDate.Day);
+            string month = getCorrectDayOrMonth(todayDate.Month);
+            string date = $"{todayDate.Year}-{month}-{day}";
+            return date;
+        }
+        private static string getCorrectDayOrMonth(int dayOrMonth)
+        {
+            return dayOrMonth >= 10 ? dayOrMonth.ToString() : $"0{dayOrMonth}";
+        }
+    }
+}
