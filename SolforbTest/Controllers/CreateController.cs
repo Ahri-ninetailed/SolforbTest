9f03d7d00be54d97e67adb0aafe82db4dc11105a
Ahri-ninetailed <vadim.halyapin@yandex.ru>
2023-03-10 14:21:54 +0300

    Add create page

---
+++ SolforbTest/Controllers/CreateController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SolforbTest.Models;
+using System.Diagnostics;
+
+namespace SolforbTest.Controllers
+{
+    public class CreateController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public CreateController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
