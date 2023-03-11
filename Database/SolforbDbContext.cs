2f90436809a3eb669e9ed611f030af286464be47
Ahri-ninetailed <vadim.halyapin@yandex.ru>
2023-03-12 00:26:00 +0300

    Add unique constraint

--- Database/SolforbDbContext.cs
+++ Database/SolforbDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using Database.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using System.Security.Principal;
 
 namespace Database
 {
@@ -21,6 +22,13 @@ namespace Database
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlite(configuration.GetConnectionString("SqliteConnection"));
             }
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Order>()
+                .HasIndex(o => new { o.ProviderId, o.Number })
+                .IsUnique(true);
+            
         }
     }
 }
