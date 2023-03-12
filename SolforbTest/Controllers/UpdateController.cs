30877b3125fd8a57529d0c66e6b6fbfd519c6d94
Ahri-ninetailed <vadim.halyapin@yandex.ru>
2023-03-12 16:54:13 +0300

    Add update order

--- SolforbTest/Controllers/UpdateController.cs
+++ SolforbTest/Controllers/UpdateController.cs
@@ -1,5 +1,6 @@
 ﻿using Database;
 using Database.Models;
+using Database.Updaters;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SolforbTest.Controllers
@@ -13,6 +14,28 @@ namespace SolforbTest.Controllers
         {
             Order order = await solforbDbContext.GetOrderByIdAsync(orderId);
             return View("Order", order);
+        }
+        [Route("Update/Order/{orderId}")]
+        [HttpPost]
+        public async Task<IActionResult> Order(int orderId, Order order)
+        {
+            if (!isOrderValidate(order))
+            {
+                fillOrderViewOfValidationErrors(order);
+                return View("Order", order);
+            }
+            Order anotherOrder = await solforbDbContext.GetOrderByProviderAndNumberAsync(order.ProviderId, order.Number);
+            if (anotherOrder is not null)
+            {
+                string errorMessage = "Заказ от такого поставщика уже существует";
+                ViewBag.NumberLabel = errorMessage;
+                ViewBag.ProviderLabel = errorMessage;
+                return View("Order", order);
+            }
+            Order foundOrder = await solforbDbContext.GetOrderByIdAsync(orderId);
+            foundOrder.UpdateOrder(order);
+            await solforbDbContext.UpdateOrderAsync(foundOrder);
+            return Redirect($"~/Order/{orderId}");
         }
     }
 }
