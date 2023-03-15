f6165fed73b91501a920a48a8ce500ce756022cf
Халяпин Вадим <vadim.halyapin@yandex.ru>
2023-03-15 18:27:07 +0300

    Add models

---
+++ SolforbTest/Models/OrderItem.cs
@@ -0,0 +1,12 @@
+﻿namespace SolforbTest.Models
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public string OrderNumber { get; set; }
+        public int OrderId { get; set; }
+        public string Name { get; set; }
+        public decimal Quantity { get; set; }
+        public string Unit { get; set; }
+    }
+}
